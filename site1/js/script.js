fd19ecf9613263859832bf7a69804555a94329ea
Alex <alexvolkovd@gmail.com>
2021-02-08 21:34:49 +0400

    change v 0.1

--- site1/js/script.js
+++ site1/js/script.js
@@ -2,4 +2,18 @@ $(document).ready(() => {
     
     /// More ...
     
+    $("#exit").hide();
+
+    var Entered;
+
+    $("#enter").on("click", ()=>{
+        $("#exit").show();
+        $("#enter").hide();
+    });
+
+    $("#exit").on("click", ()=>{
+        $("#enter").show();
+        $("#exit").hide();
+    });
+
 });
