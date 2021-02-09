d4d942f29e6a6f8697b759b2ab4376a2220fd06b
Alex <alexvolkovd@gmail.com>
2021-02-09 14:06:45 +0400

    change v 0.1

--- site1/js/script.js
+++ site1/js/script.js
@@ -9,11 +9,13 @@ $(document).ready(() => {
     $("#enter").on("click", ()=>{
         $("#exit").show();
         $("#enter").hide();
+        $("#menu").show();
     });
 
     $("#exit").on("click", ()=>{
         $("#enter").show();
         $("#exit").hide();
+        $("#menu").hide();
     });
 
 });
