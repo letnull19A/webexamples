5c0e3304b8ca3740723e4004183cf199db291e31
Alex <alexvolkovd@gmail.com>
2021-02-27 10:57:37 +0400

    New version of factory

--- factory/Gulpfile.js
+++ factory/Gulpfile.js
@@ -1,16 +1,32 @@
 'use strict';
  
 var gulp = require('gulp'),
-    sass = require('gulp-sass');
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync');
  
+
+browserSync.create();
 sass.compiler = require('node-sass');
  
-gulp.task('scss', function () {
+
+gulp.task('serve', () => {
+  
+  browserSync.init({
+    server: "./dist",
+    online: true,
+    tunnel: "test",
+  });
+
+  gulp.watch(['./src/scss/*.scss', "!./src/scss/_*.scss"], gulp.parallel('scss'));
+  gulp.watch("./dist/**").on("change", browserSync.reload);
+});
+
+gulp.task('scss', () => {
   return gulp.src(['./src/scss/*.scss', '!./src/scss/_*.scss'])
     .pipe(sass())
     .pipe(gulp.dest('./dist/css'));
 });
  
-gulp.task('scss:watch', function () {
-  gulp.watch('./src/scss/*.scss', gulp.parallel('scss'));
+gulp.task('scss:watch', () => {
+  gulp.watch(['./src/scss/*.scss', "!./src/scss/_*.scss"], gulp.parallel('scss'));
 });
